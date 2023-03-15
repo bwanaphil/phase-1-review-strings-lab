d30513658850500ef31a3a6371a91c72650e8951
bwanaphil <pirannahr@yahoo.com>
2023-03-15 14:11:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'GRACE HOPPER';
+const firstInitial = 'G';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser} !`;
+const shortGreeting = `Welcome, ${firstInitial}!`;
